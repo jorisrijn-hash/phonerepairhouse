8f13acb366ab54f8ec6e7d67dbcee50c5152d837
Joris van Rijn <joris@joriss-mbp.home>
2026-08-14 11:18:59 +0200

    Terugknop bij enkele serie gefixt; 30 iPad-modellen met echte prijzen

---
+++ PRIJZEN-IPAD-BRON.js
@@ -0,0 +1,40 @@
+/* ============================================================
+   ECHTE PRIJZEN iPAD (aug 2026)
+   Per model benoemde velden, omdat de regels per generatie
+   verschillen (los glas, home button, audiojack).
+   ============================================================ */
+const PRIJZEN_IPAD={
+"iPad Pro 13 (2024)":{scherm:949,accu:285,laad:115,camVoor:135,camAchter:149,speaker:115,micro:99,aanuit:115,volume:115,tril:99,wifi:115,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 12.9 (2022)":{scherm:369,accu:115,laad:99,camVoor:95,camAchter:125,speaker:125,micro:99,aanuit:99,volume:99,tril:95,wifi:99,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 11 (2022)":{scherm:299,accu:149,laad:149,camVoor:89,camAchter:125,speaker:95,micro:89,aanuit:99,volume:99,tril:85,wifi:99,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 12.9 (2021)":{scherm:369,accu:115,laad:99,camVoor:99,camAchter:125,speaker:129,micro:89,aanuit:95,volume:95,tril:89,wifi:95,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 11 (2021)":{scherm:299,accu:109,laad:95,camVoor:99,camAchter:119,speaker:99,micro:89,aanuit:89,volume:89,tril:85,wifi:95,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 12.9 (2020)":{scherm:279,accu:109,laad:89,camVoor:89,camAchter:119,speaker:119,micro:85,aanuit:85,volume:85,tril:85,wifi:95,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 11 (2020)":{scherm:299,accu:99,laad:95,camVoor:89,camAchter:109,speaker:99,micro:85,aanuit:89,volume:89,tril:85,wifi:95,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 12.9 (2018)":{scherm:279,accu:85,laad:69,camVoor:69,camAchter:85,speaker:95,micro:65,aanuit:65,volume:65,tril:65,wifi:69,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Pro 11 (2018)":{scherm:299,accu:85,laad:69,camVoor:85,camAchter:89,speaker:75,micro:65,aanuit:65,volume:65,tril:65,wifi:69,water:49,onderzoek:15,software:25,moederbord:135},
+
+"iPad Air 13 (2025)":{scherm:299,accu:199,laad:125,camVoor:125,camAchter:135,speaker:115,micro:99,aanuit:99,volume:99,tril:99,wifi:115,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Air 11 (2025)":{scherm:249,accu:199,laad:115,camVoor:125,camAchter:135,speaker:99,micro:99,aanuit:99,volume:99,tril:89,wifi:135,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Air 13 (2024)":{scherm:299,accu:115,laad:115,camVoor:115,camAchter:125,speaker:115,micro:99,aanuit:99,volume:99,tril:95,wifi:115,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Air 11 (2024)":{scherm:249,accu:115,laad:115,camVoor:115,camAchter:125,speaker:95,micro:95,aanuit:95,volume:95,tril:85,wifi:115,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Air 5 (2022)":{scherm:229,accu:115,laad:99,camVoor:99,camAchter:99,speaker:99,micro:95,aanuit:95,volume:95,tril:89,wifi:115,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Air 4":{scherm:229,accu:115,laad:85,camVoor:85,camAchter:95,speaker:85,micro:79,aanuit:85,volume:85,tril:79,wifi:99,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Air 3 (2019)":{scherm:229,accu:85,laad:75,home:65,camVoor:65,camAchter:99,speaker:75,micro:65,aanuit:75,volume:75,tril:65,audio:65,wifi:85,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Air 2":{scherm:149,accu:75,laad:65,home:59,camVoor:59,camAchter:75,speaker:65,micro:65,aanuit:65,volume:65,tril:65,audio:65,wifi:75,water:49,onderzoek:15,software:25,moederbord:135},
+
+"iPad A16 11 (2025)":{glas:139,lcdGlas:199,accu:199,laad:115,camVoor:89,camAchter:99,speaker:109,micro:89,aanuit:99,volume:99,tril:85,wifi:99,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad 2022":{glas:139,lcdGlas:189,accu:99,laad:89,camVoor:85,camAchter:99,speaker:95,micro:85,aanuit:89,volume:85,tril:85,wifi:95,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad 2021":{glas:119,lcdGlas:179,accu:95,laad:85,home:65,camVoor:79,camAchter:99,speaker:75,micro:75,aanuit:79,volume:79,tril:75,audio:69,wifi:75,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad 2020":{glas:109,lcdGlas:179,accu:85,laad:79,home:65,camVoor:69,camAchter:99,speaker:75,micro:75,aanuit:75,volume:75,tril:69,audio:69,wifi:75,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad 2019":{glas:99,lcdGlas:179,accu:85,laad:75,home:65,camVoor:65,camAchter:99,speaker:75,micro:75,aanuit:75,volume:75,tril:65,audio:65,wifi:75,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad 2018":{glas:89,lcdGlas:149,accu:79,laad:65,home:55,camVoor:59,camAchter:95,speaker:65,micro:65,aanuit:65,volume:65,tril:65,audio:65,wifi:75,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad 4":{glas:69,lcdGlas:99,accu:75,laad:59,home:59,camVoor:59,camAchter:65,speaker:59,micro:59,aanuit:59,volume:59,tril:59,audio:59,wifi:65,water:49,onderzoek:15,software:25,moederbord:135},
+
+"iPad Mini 7 (2024)":{scherm:329,accu:195,laad:105,camVoor:95,camAchter:95,speaker:105,micro:89,aanuit:99,volume:99,tril:89,wifi:99,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Mini 6 (2021)":{scherm:329,accu:115,laad:99,camVoor:89,camAchter:89,speaker:99,micro:89,aanuit:99,volume:85,tril:85,wifi:99,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Mini 5":{scherm:179,accu:75,laad:75,home:69,camVoor:69,camAchter:95,speaker:69,micro:69,aanuit:65,volume:65,tril:65,audio:65,wifi:75,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Mini 4":{scherm:169,accu:69,laad:69,home:69,camVoor:49,camAchter:49,speaker:49,micro:65,aanuit:59,volume:59,tril:59,audio:65,wifi:69,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Mini 3":{glas:79,lcdGlas:99,accu:69,laad:65,home:45,camVoor:45,camAchter:49,speaker:49,micro:45,aanuit:49,volume:49,tril:45,audio:49,wifi:49,water:49,onderzoek:15,software:25,moederbord:135},
+"iPad Mini 2":{glas:79,lcdGlas:99,accu:69,laad:65,home:45,camVoor:39,camAchter:49,speaker:49,micro:39,aanuit:49,volume:49,tril:49,audio:49,wifi:49,water:49,onderzoek:15,software:25,moederbord:135}
+};
